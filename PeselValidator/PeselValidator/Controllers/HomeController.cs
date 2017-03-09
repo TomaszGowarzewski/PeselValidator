81de344951679508ff4556ff87f732f0c9460f59
Bubu <Bubu@DESKTOP-05UP5JL>
2017-03-09 01:29:27 +0100

    Forms, Identity validator was added

---
+++ PeselValidator/PeselValidator/Controllers/HomeController.cs
@@ -0,0 +1,100 @@
+﻿using PeselValidator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeselValidator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(PersonModel personResponse, string checkCorrectnessOfDateAndID, string checkDateAndGender)
+        {
+            if (personResponse == null)
+            {
+                throw new ArgumentNullException(nameof(personResponse));
+            }
+            else if (ModelState.IsValid)
+            {
+                int[] IdentityNumberArray = personResponse.IdentityNumber.Select(x => int.Parse(x.ToString())).ToArray();
+                IdentityHelper helper = new IdentityHelper(IdentityNumberArray);
+
+                DateTime dateTimeFromIdentity = helper.GetDate();
+                string genderFromIdentity = helper.GetGender();
+
+                if (!String.IsNullOrEmpty(checkCorrectnessOfDateAndID))
+                {
+
+                    if (ValidateIdentityNumber(helper))
+                    {
+                        PersonModel newPersonModel = new PersonModel();
+                        newPersonModel.DateOfBirth = dateTimeFromIdentity.ToLongDateString();
+                        newPersonModel.Gender = genderFromIdentity;
+                        newPersonModel.Name = personResponse.Name;
+                        return View("InformationDisplay",newPersonModel);
+                    }
+                    return View("WrongInformation");
+                }
+               else if (!String.IsNullOrEmpty(checkDateAndGender))
+                {
+                    if (CompareDates(dateTimeFromIdentity, DateTime.Parse(personResponse.DateOfBirth)) && CompareGender(genderFromIdentity, personResponse.Gender))
+                    {
+                        return View("DateAndGenderEquals");
+                    }
+                    return View("DateAndGenderNotEquals");
+                }
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+
+
+        private bool CompareGender(string genderFromIdentityNumber, string genderFromResponse)
+        {
+            if (string.Equals(genderFromIdentityNumber, genderFromResponse))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        private bool CompareDates(DateTime dateFromIdentityNumber, DateTime dateFromResponse)
+        {
+            if (DateTime.Equals(dateFromIdentityNumber, dateFromResponse))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool ValidateIdentityNumber(IdentityHelper helper)
+        {
+
+            if (helper.ValidateIdentity())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private ViewResult CheckCorrectnessDateAndID(PersonModel guestResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PeselValidator/PeselValidator/Controllers/IdentityHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeselValidator.Controllers
+{
+    internal class IdentityHelper
+    {
+        private int[] identityNumberArray;
+
+        public IdentityHelper(int[] identityNumberArray)
+        {
+            if (identityNumberArray.Length != 11)
+            {
+                throw new ArgumentException(nameof(identityNumberArray));
+            }
+            this.identityNumberArray = identityNumberArray;
+        }
+
+        public bool ValidateIdentity()
+        {
+          return ( GetSumDigit() == identityNumberArray[10]);         
+        }
+
+        public DateTime GetDate()
+        {
+
+            int day = identityNumberArray[4] * 10 + identityNumberArray[5];
+            int year = 0;
+            int month = 0;
+
+            if (identityNumberArray[2] == 0 || identityNumberArray[2] == 1)
+            {
+                month = identityNumberArray[2] * 10 + identityNumberArray[3];
+                year = 1900;
+            }
+            else if (identityNumberArray[2] == 2 || identityNumberArray[2] == 3)
+            {
+                month = identityNumberArray[2] * 10 + identityNumberArray[3] - 20;
+                year = 2000;
+            }
+            else if (identityNumberArray[2] == 4 || identityNumberArray[2] == 5)
+            {
+                month = identityNumberArray[2] * 10 + identityNumberArray[3] - 40;
+                year = 2100;
+            }
+            else if (identityNumberArray[2] == 6 || identityNumberArray[2] == 7)
+            {
+                month = identityNumberArray[2] * 10 + identityNumberArray[3] - 60;
+                year = 2200;
+            }
+            else if (identityNumberArray[2] == 8 || identityNumberArray[2] == 9)
+            {
+                month = identityNumberArray[2] * 10 + identityNumberArray[3] - 80;
+                year = 1800;
+            }
+            year += identityNumberArray[0] * 10 + identityNumberArray[1];
+
+            return new DateTime(year, month, day);
+           
+        }
+
+        public string GetGender()
+        {
+            if (identityNumberArray[9] % 2 == 0)
+            {
+                return "Female";
+            }
+            else
+            {
+                return "Male";
+            }
+        }
+
+        private int GetSumDigit()
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, };
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += (weights[i] * identityNumberArray[i]);
+
+            }
+            sum %= 10;
+            sum = 10 - sum;
+            return sum;
+        }
+    }
+}
